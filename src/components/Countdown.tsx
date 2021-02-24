daa595c2facb7184979ddc265ad3e676fe658abf
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-24 17:19:58 -0300

    context api

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+  const contextData = useContext(ChallengesContext);
+  console.log(contextData);
+
   const [time, setTime] = useState(0.1 * 60);
   const [isActive, setisActive] = useState(false);
   const [hasFinished, sethasFinished] = useState(false);
