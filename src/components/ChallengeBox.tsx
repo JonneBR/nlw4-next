ccba5fe1b4d561a501af17c76952816fca67f7d0
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-25 09:08:12 -0300

    function start new challenge working

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,18 +1,21 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge } = useContext(ChallengesContext);
+  // console.log(activeChallenge);
 
   return (
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src='icons/body.svg' alt='Body img' />
+            <img src={`icons/${activeChallenge.type}.svg`} alt='Body img' />
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 5 minutos.</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
