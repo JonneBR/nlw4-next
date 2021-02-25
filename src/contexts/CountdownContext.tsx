93725f1f3db79a8b78529103af9d8d9c817243c1
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-25 10:42:38 -0300

    basic context structure

---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,15 @@
+import { createContext, ReactNode } from 'react';
+
+interface CountdownContextData {}
+
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
+const CountdownContext = createContext({} as CountdownContextData);
+
+export function CountdownProvider({ children }) {
+  return (
+    <CountdownContext.Provider value={{}}>{children}</CountdownContext.Provider>
+  );
+}
