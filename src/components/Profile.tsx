ea88d63e5459c0067fdae8d22d6278c86ce20e6d
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-25 10:02:26 -0300

    level up context api

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src='https://github.com/jonnebr.png' alt='João Marcos Gonçalves' />
@@ -9,7 +12,7 @@ export function Profile() {
 
         <p>
           <img src='icons/level.svg' alt='Level' />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
