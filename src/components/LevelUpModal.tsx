7cc6ec9abf029713eea38d2b3dc8a8d7cccddd49
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-26 17:41:40 -0300

    level up modal function

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -3,16 +3,16 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/LevelUpModal.module.css';
 
 export function LevelUpModal() {
-  const {} = useContext(ChallengesContext);
+  const { level, closeLevelUpModal } = useContext(ChallengesContext);
 
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
-        <header>2</header>
+        <header>{level}</header>
         <strong>Parábens</strong>
         <p>Você alcançou um novo level.</p>
 
-        <button type='button'>
+        <button type='button' onClick={closeLevelUpModal}>
           <img src='/icons/close.svg' alt='Fechar modal' />
         </button>
       </div>
